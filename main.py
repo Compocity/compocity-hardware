926de270b0e7158de3f4c525c520547c1e7ce4f0
debrecenid <debreceindavid0311@gmail.com>
2020-08-26 14:43:27 +0100

    hx711 added

---
+++ main.py
@@ -0,0 +1,88 @@
+import os
+import glob
+import time
+import Adafruit_DHT
+
+# Import SPI library (for hardware SPI) and MCP3008 library.
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_MCP3008
+
+# Software SPI configuration:
+CLK = 11
+MISO = 9
+MOSI = 10
+CS = 8
+mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
+
+DHT_SENSOR = Adafruit_DHT.DHT11
+DHT_PIN = 18
+
+# These tow lines mount the device:
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+
+base_dir = '/sys/bus/w1/devices/'
+# Get all the filenames begin with 28 in the path base_dir.
+device_folder = glob.glob(base_dir + '28*')[0]
+device_file = device_folder + '/w1_slave'
+
+
+def read_rom():
+    name_file = device_folder+'/name'
+    f = open(name_file, 'r')
+    return f.readline()
+
+
+def read_temp_raw():
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+
+
+def read_temp():
+    lines = read_temp_raw()
+    # Analyze if the last 3 characters are 'YES'.
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temp_raw()
+    # Find the index of 't=' in a string.
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        # Read the temperature .
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+        temp_f = temp_c * 9.0 / 5.0 + 32.0
+        return temp_c, temp_f
+
+
+def asd():
+    result = 'hello'
+    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+    if humidity is not None and temperature is not None:
+        result = humidity
+    return "Temp={0:0.1f}C Humidity={1:0.1f}%".format(
+        temperature, humidity)
+
+
+def dsa():
+    values = [0]*8
+    for i in range(8):
+        # The read_adc function will get the value of the specified channel (0-7).
+        values[i] = mcp.read_adc(i)
+    # Print the ADC values.
+    return'| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*values)
+
+
+print(' rom: ' + read_rom())
+print('Reading MCP3008 values, press Ctrl-C to quit...')
+# Print nice channel column headers.
+print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*range(8)))
+print('-' * 57)
+
+while True:
+    print(' C=%3.3f  F=%3.3f' % read_temp())
+    print(asd())
+    print(dsa())
+time.sleep(1)
+
