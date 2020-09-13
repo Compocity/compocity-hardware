c487e7ffd787c87c89e2b6ec0ad508be10738ccf
debrecenid <debreceindavid0311@gmail.com>
2020-09-13 15:02:26 +0100

    t7commit

--- open.py
+++ open.py
@@ -2,15 +2,15 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(16, GPIO.IN)
+GPIO.setup(19, GPIO.IN)
 
 try:
     while True:
-        if((not GPIO.input(16))):
+        if((not GPIO.input(19))):
             print('csukd le a compocityt')
-            while not GPIO.input(16):
-                time.sleep(500)
-                print(GPIO.input(16))
+            while (not GPIO.input(19)):
+                time.sleep(1)
+                print(GPIO.input(19))
             print('scanneld be a pontokat')
 
 except:
